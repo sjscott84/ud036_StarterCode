f178d5df9a5625e902168f5bc18ac8ef4b8febb9
Sarah Scott <s.j.scott84@gmail.com>
2017-04-18 10:37:59 -0700

    Add comments and amend formatting

--- media.py
+++ media.py
@@ -1,8 +1,12 @@
 import webbrowser
 
 class Movie():
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """
+    Creates a movie object with the variables title, storyline,
+    poster_image_url, trailer_youtube_url and a show_trailer method.   
+    """
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -11,5 +15,3 @@ class Movie():
     # This function opens the youtube link for the trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
-        
